79a867da3277e32c5ccb84f30311e4d4b6a9941f
Publo <111525871+Candymanzz@users.noreply.github.com>
2024-12-21 00:50:46 +0300

    DbSet properties

--- Db/AppDbContext.cs
+++ Db/AppDbContext.cs
@@ -9,7 +9,15 @@ namespace juridical_api.Db
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<CasesEntities> Cases { get; set; }
         public DbSet<ClientsEntities> Clients { get; set; }
+        public DbSet<ContractsEntities> Contracts { get; set; }
+        public DbSet<CourtHearingsEntities> CourtHearings { get; set; }
+        public DbSet<DocumentsEntities> Documents { get; set; }
+        public DbSet<LawyersEntities> Lawyers { get; set; }
+        public DbSet<PaymentsEntities> Payments { get; set; }
+        public DbSet<ReviewsEntities> Reviews { get; set; }
+        public DbSet<TasksEntities> Tasks { get; set; }
 
         public AppDbContext(DbContextOptions options) : base(options)
         {
