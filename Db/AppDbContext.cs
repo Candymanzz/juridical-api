275d44256871021e2098a686ef65371106721e3a
Publo <111525871+Candymanzz@users.noreply.github.com>
2024-12-22 02:18:15 +0300

    add on model creating

--- Db/AppDbContext.cs
+++ Db/AppDbContext.cs
@@ -22,5 +22,10 @@ namespace juridical_api.Db
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
