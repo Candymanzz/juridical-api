174161c2df37e12c2cdc458463109bbe2e0be939
Publo <111525871+Candymanzz@users.noreply.github.com>
2024-12-30 00:31:19 +0300

    Update CasesConfiguration.cs

--- Configuration/CasesConfiguration.cs
+++ Configuration/CasesConfiguration.cs
@@ -37,7 +37,7 @@ namespace juridical_api.Configuration
                     Id = new Guid("8B9658B8-49C1-423A-A807-71D1706710E5"),
                     CaseName = "Case name 1",
                     Description = "Some description",
-                    OpeningDate = new DateTime(2000, 11, 220),
+                    OpeningDate = new DateTime(2000, 11, 22),
                     ClientId = new Guid("8B9658B8-49C1-423A-A807-71D1706710E1"),
                     LawyerId = new Guid("8B9658B8-49C1-423A-A807-71D1706710E2")
                 }
