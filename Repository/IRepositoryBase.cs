98a43bd2b27f509e0d70017deb15348949800974
Publo <111525871+Candymanzz@users.noreply.github.com>
2024-12-31 14:31:26 +0300

    Create IRepositoryBase.cs

---
+++ Repository/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace juridical_api.Repository
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
