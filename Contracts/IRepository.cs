d8cae1535713689fc0988efd3ed6fb3ee04d9c46
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-01-03 02:55:06 +0300

    interface for repository

---
+++ Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace juridical_api.Contracts
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T?> Get(Guid id);
+        Task Create(T item);
+        Task Update(Guid id, T item);
+        Task Delete(Guid id);
+        bool StatusDispose();
+    }
+}
