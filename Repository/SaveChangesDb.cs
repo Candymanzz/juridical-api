087f12681a0a670ebd9ea92714f2cbf59b1404dc
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-01-03 02:54:27 +0300

    Create SaveChangesDb.cs

---
+++ Repository/SaveChangesDb.cs
@@ -0,0 +1,21 @@
+﻿using juridical_api.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace juridical_api.Repository
+{
+    public class SaveChangesDb
+    {
+        public async Task Save(AppDbContext appDbContext)
+        {
+            try
+            {
+                await appDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.Error.WriteLine($"Error saving changes: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
